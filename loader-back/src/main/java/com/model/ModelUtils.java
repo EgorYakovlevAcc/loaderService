b8ab51da48c4f761a024410c741ca92c861340bd
Egor <yakovlev.es@phystech.edu>
2020-07-20 14:35:48 +0300

    remove fix

--- loader-back/src/main/java/com/model/ModelUtils.java
+++ loader-back/src/main/java/com/model/ModelUtils.java
@@ -2,18 +2,23 @@ package com.model;
 
 import com.model.order.Order;
 import com.model.user.Porter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.Time;
 import java.util.Calendar;
 
 public class ModelUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ModelUtils.class);
     public static boolean filterPortersByTimetable(Porter porter, Order order) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(order.getOrderDate());
         Integer orderDayId = calendar.get(Calendar.DAY_OF_WEEK);
+        LOGGER.info("filterPortersByTimetable: orderDayId = {}", orderDayId);
 
-        return porter.getTimeTables().stream()
+        boolean result =  porter.getTimeTables().stream()
                 .filter(timeTable -> timeTable.getDay().equals(orderDayId))
                 .anyMatch(timeTable -> (timeTable.getStart().before(order.getTime())) && (timeTable.getFinish().after(order.getTime())));
+        LOGGER.info("filterPortersByTimetable: result = {}", result);
+        return result;
     }
 }
